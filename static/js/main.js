15bb13ed1d61862eee765b58df03a8b04efcc5e2
Samantha <ssstrieu@gmail.com>
2015-11-12 17:46:47 +0000

    dropin students cannot be absent. reset the absence field

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,14 @@
 $( document ).ready(function(){
     console.log('=======document is now ready!!!!!======');
     
-    // $( "#datepicker" ).datepicker();
-    
     $('#clearFormBtn').on('click', function(){
         //This returns the form to default entries
         console.log('=======Clear Form clicked======');
         $('#logForm')[0].reset();
     });
 
-    
+    // When the absence button is clicked, this code prefills the form with 'N/A' or absences values
     $('#studentAbsentBtn').on('click', function(){
-        // When the absence button is clicked, this code prefills the form with 'N/A'
         console.log('=======Absence clicked======');
         $('#matchedStudent').prop('checked',true);
         $('#absenceCheck').css('display','inline').prop('checked', true);
@@ -21,7 +18,13 @@ $( document ).ready(function(){
         $('#scholar_rank').val('0');
         $('#notes').val('Student absent.')
         
-    })
+    });
+    
+    // sets Absence to 0 if Dropin Student is selected
+    $('#dropinStudent').on('click', function(){
+        console.log('=======Dropin student cannot be absent. Absence reset======');
+        $('#absenceCheck').prop('checked', false); 
+    });
         
    
  }); //document-ready close
