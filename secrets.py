80f7f68a252c304e44210da60974035abb58f460
Samantha <ssstrieu@gmail.com>
2016-01-11 04:03:00 +0000

    rm gitignore

---
+++ secrets.py
@@ -0,0 +1,10 @@
+users = {"build": "volunteer","yap":'volunteer'} # httpauth
+
+# id1='build'
+    
+# if users.keys(id1)=='build':
+#     print 'found ',id1
+
+GOOGLE_CLIENT_ID = '834450588178-tmqi729odq8h0vkbik3rk9rm4no4aqgp.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'epOQT9krb6yKrkj7tu74PBEy'
+secret='flask secret key'
