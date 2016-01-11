99a06f5180ed04359ac2bcb77866992477c392f1
Samantha <ssstrieu@gmail.com>
2016-01-11 02:26:16 +0000

    new flow for YAP

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from secrets import *
 
 app = Flask(__name__) 
 app.secret_key = secret
+
+
 auth = HTTPBasicAuth()
 
 
@@ -18,39 +20,65 @@ def get_pw(username):
 @app.route('/')
 @auth.login_required
 def index():
-    return render_template('index.html')
-
+    if auth.username()=='build':
+        site='Longfellow'
+        return render_template('index.html',site=site)
+    if auth.username()=='yap':
+        site='Young Adult Project'
+        return render_template('index2.html',site=site)
 
 @app.route('/submitLog',methods=['POST'])
 def submitform():
-    try:
-        site=request.form.get('site')
-        mentor=request.form.get('mentor')
-        scholar=request.form.get('scholar')
-        absence=0
-        if request.form.get('absenceCheck'): #absenceCheck returns 'checked' or None
-            absence=1
-        isDropin=request.form.get('isDropin')
-        math_topic=request.form.get('math_topic')
-        duration=int(request.form.get('duration'))
-        mentor_rank=int(request.form.get('mentor_rank'))
-        scholar_rank=int(request.form.get('scholar_rank'))
-        note=request.form.get('notes')
-        print 'Form Data =====>',site,mentor,scholar,absence,isDropin,duration, math_topic, mentor_rank,scholar_rank, note
-        print 'NOT YET written to log'
-        writeToLog(site,mentor,scholar,absence,isDropin,duration, math_topic, mentor_rank,scholar_rank, note)
-        print 'SUCCESSFULLY Written to log'
-        post_success=True
-        message='Thank you for submitting your activity.'
-     
-        return render_template('index.html',post_success=post_success,message=message)
-    except: 
-        print 'FAILED'
-        post_success=False
-        message='Opps! Missing form data. Please fill out all the fields and try again.'
-        return render_template('index.html',post_success=post_success,message=message)
-    
+    if auth.username()=='build':
+        try:
+            site='Longfellow'
+            mentor=request.form.get('mentor')
+            scholar=request.form.get('scholar')
+            absence=0
+            if request.form.get('absenceCheck'): #absenceCheck returns 'checked' or None
+                absence=1
+            isDropin=request.form.get('isDropin')
+            math_topic=request.form.get('math_topic')
+            duration=int(request.form.get('duration'))
+            mentor_rank=int(request.form.get('mentor_rank'))
+            scholar_rank=int(request.form.get('scholar_rank'))
+            note=request.form.get('notes')
+            # print 'Form Data =====>',site,mentor,scholar,absence,isDropin,duration, math_topic, mentor_rank,scholar_rank, note
+            # print 'NOT YET written to log'
+            writeToLog(site,mentor,scholar,absence,isDropin,duration, math_topic, mentor_rank,scholar_rank, note)
+            # print 'SUCCESSFULLY Written to log'
+            post_success=True
+            message='Thank you for submitting your activity.'
+            return render_template('index.html',post_success=post_success,message=message)
+        except: 
+            print 'FAILED'
+            post_success=False
+            message='Opps! Missing form data. Please fill out all the fields and try again.'
+            return render_template('index.html',post_success=post_success,message=message)
+    if auth.username()=='yap':
+        try:
+            site="Young Adult Project"
+            mentor=request.form.get('mentor')
+            scholar=request.form.get('scholar')
+            absence="N/A"
+            isDropin="Yes"
+            math_topic=request.form.get('math_topic')
+            duration=int(request.form.get('duration'))
+            mentor_rank="N/A"
+            scholar_rank= "N/A"
+            note=request.form.get('notes')
+            # print 'Form Data =====>',site,mentor,scholar,absence,isDropin,duration, math_topic, mentor_rank,scholar_rank, note
+            # print 'NOT YET written to log'
+            writeToLog(site,mentor,scholar,absence,isDropin,duration, math_topic, mentor_rank,scholar_rank, note)
+            # print 'SUCCESSFULLY Written to log'
+            post_success=True
+            message='Thank you for submitting your activity.'
+            return render_template('index2.html',post_success=post_success,message=message)
+        except: 
+            post_success=False
+            message='Opps! Missing form data. Please fill out all the fields and try again.'
+            return render_template('index2.html',post_success=post_success,message=message)
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080))) #for running in cloud9
+    app.run(debug=True,host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080))) #for running in cloud9
     # app.run(debug=True, port=5000)  
